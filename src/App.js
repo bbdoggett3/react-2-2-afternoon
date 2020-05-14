55cdb90b3c8139dd6518984966a7af6ef867fe54
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-14 14:47:33 -0600

    finished my CSS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component  {
+constructor() {
+  super();
+
+  this.state = {
+    person: []
+  }
+}
+
+  render() {
+    return (
+      <div className="App">
+        <header className = 'nav-header'>
+          <h3>Home</h3>
+        </header>
+        <div className= "person-container">
+          <div className = "page-counter">1/25</div>
+          <div className= "person-info-child">
+              <h4>Firstname</h4>
+              <br></br>
+              <p><span>From:</span> example: Liverpool, United Kingdom</p>
+              <p><span>Job Title:</span> example: Liverpool, United Kingdom</p>
+              <p><span>Employeer:</span> example: Liverpool, United Kingdom</p>
+              <br></br>
+              <p><span>Favorite Movies:</span></p>
+                <ol>
+                  <li>Back To the Future</li>
+                  <li>Tron</li>
+                  <li>Men in Black</li>
+                </ol>
+          </div>
+        </div>
+        <div className="nav-bar-below">
+          <button className= "prev-next">Previous</button>
+          <div>
+            <button className = "buttons">Edit</button>
+            <button className = "buttons">Delete</button>
+            <button className = "buttons">New</button>
+          </div>
+          <button className= "prev-next">Next ></button>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
